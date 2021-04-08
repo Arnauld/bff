4ea557e80d0da5c60e023e58f14749b112a182bd
Arnauld <arnauld.loyer@ratpsmartsystems.com>
2021-04-08 09:38:46 +0200

    feat: create many generic users

--- devenv/init-db/init.sql
+++ devenv/init-db/init.sql
@@ -7,13 +7,23 @@ CREATE TABLE IF NOT EXISTS users (
     data           JSONB NOT NULL DEFAULT '{}'::JSONB
 );
 
-CREATE UNIQUE INDEX idx_users_username ON users USING GIN (username);
-CREATE UNIQUE INDEX idx_users_email    ON users USING GIN ((lower(data->'email')));
+CREATE UNIQUE INDEX idx_users_username ON users(username);
+CREATE UNIQUE INDEX idx_users_email    ON users((lower(data->>'email')));
 
 CREATE extension pgcrypto;
 
 INSERT INTO users (username, password, data) VALUES
     ('carmen', crypt('carmen_p', gen_salt('bf', 10)), '{"email":"carmen@tra.vis", "firstName":"Carmen", "lastName":"McCallum"}'::jsonb),
-    ('vlad', crypt('vlap_p', gen_salt('bf', 10)), '{"firstName":"Vlad", "lastName":"Nyrki"}'::jsonb),
-    ('travis', crypt('travis_p', gen_salt('bf', 10)), '{"email":"travis@tra.vis", "firstName":"Steve", "lastName":"Travis"}'::jsonb),
+    ('vlad',   crypt('vlap_p',   gen_salt('bf', 10)), '{                          "firstName":"Vlad",   "lastName":"Nyrki"   }'::jsonb),
+    ('travis', crypt('travis_p', gen_salt('bf', 10)), '{"email":"travis@tra.vis", "firstName":"Steve",  "lastName":"Travis"  }'::jsonb),
     ('Miss Thundercat', null, '{}'::jsonb);
+
+-- Insert huge amount of users...
+-- all with the same password due to blowfish (timeconsumiiing) purpose
+INSERT INTO users(username, password) 
+    SELECT 
+        to_char(g, 'fm000000'),
+--        crypt(to_char(g, 'fm000000'), gen_salt('bf', 4)) 
+        p
+    FROM generate_series(1, 100000) as g,
+         crypt('admin', gen_salt('bf', 10)) as p;
