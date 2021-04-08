450c71fa27445c11e09e8c2fb55b3103a265ffaa
Arnauld <arnauld.loyer@gmail.com>
2021-04-08 23:11:04 +0200

    fix: invalid/missing dependencies

--- gateway/gateway.js
+++ gateway/gateway.js
@@ -18,7 +18,7 @@ const gateway = new ApolloGateway({
     return new RemoteGraphQLDataSource({
       url,
       willSendRequest({ request, context }) {
-        console.log(`${__filename}::willSendRequest(${name})`, request, console);
+        console.log(`${__filename}::willSendRequest(${name})`, request, context);
         // Passing Keycloak Access Token to services
         if (context.kauth && context.kauth.accessToken) {
           request.http.headers.set('Authorization', 'bearer ' + context.kauth.accessToken.token);
@@ -30,52 +30,56 @@ const gateway = new ApolloGateway({
   __exposeQueryPlanExperimental: true,
 });
 
-// perform the standard keycloak-connect middleware setup on our app
-const { keycloak } = configureKeycloak(app, graphqlPath);
+(async () => {
+  // perform the standard keycloak-connect middleware setup on our app
+  const { keycloak } = configureKeycloak(app, graphqlPath);
 
-// Ensure entire GraphQL Api can only be accessed by authenticated users
-app.use(playgroundPath, keycloak.protect());
+  // Ensure entire GraphQL Api can only be accessed by authenticated users
+  app.use(playgroundPath, keycloak.protect());
 
-const server = new ApolloServer({
-  gateway,
+  const server = new ApolloServer({
+    gateway,
 
-  // Apollo Graph Manager (previously known as Apollo Engine)
-  // When enabled and an `ENGINE_API_KEY` is set in the environment,
-  // provides metrics, schema management and trace reporting.
-  engine: false,
+    // Apollo Graph Manager (previously known as Apollo Engine)
+    // When enabled and an `ENGINE_API_KEY` is set in the environment,
+    // provides metrics, schema management and trace reporting.
+    engine: false,
 
-  // Subscriptions are unsupported but planned for a future Gateway version.
-  subscriptions: false,
+    // Subscriptions are unsupported but planned for a future Gateway version.
+    subscriptions: false,
 
-  // Disable default playground
-  playground: false,
+    // Disable default playground
+    playground: false,
 
-  context: ({ req }) => {
-    console.log(`ApolloServer::${req}`)
-    const kauth = new KeycloakContext({ req });
-    return { kauth };
-  }
-});
-
-// Handle custom GraphQL Playground to use dynamics header token from keycloak
-app.get(playgroundPath, (req, res, next) => {
-  const headers = JSON.stringify({
-    'X-CSRF-Token': req.kauth.grant.access_token.token,
+    context: ({ req }) => {
+      console.log(`ApolloServer::${req}`)
+      const kauth = new KeycloakContext({ req });
+      return { kauth };
+    }
   });
-  expressPlayground({
-    ...defaultPlaygroundOptions,
-    endpoint: `${graphqlPath}?headers=${encodeURIComponent(headers)}`,
-    settings: {
-      ...defaultPlaygroundOptions.settings,
-      'request.credentials': 'same-origin',
-    },
-    version: "",
-    tabs: ""
-  })(req, res, next);
-});
+  await server.start();
 
-server.applyMiddleware({ app });
+  // Handle custom GraphQL Playground to use dynamics header token from keycloak
+  app.get(playgroundPath, (req, res, next) => {
+    const headers = JSON.stringify({
+      'X-CSRF-Token': req.kauth.grant.access_token.token,
+    });
+    expressPlayground({
+      ...defaultPlaygroundOptions,
+      endpoint: `${graphqlPath}?headers=${encodeURIComponent(headers)}`,
+      settings: {
+        ...defaultPlaygroundOptions.settings,
+        'request.credentials': 'same-origin',
+      },
+      version: "",
+      tabs: ""
+    })(req, res, next);
+  });
 
-app.listen({ port }).then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}${server.graphqlPath}`);
-});
+  server.applyMiddleware({ app });
+
+  app.listen({ port }, () => {
+    console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`);
+  });
+
+})();
